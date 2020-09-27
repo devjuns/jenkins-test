d2ac7364baad9e2f4882b2cdf873a25b74e96235
junsoo <wnstn212@gmail.com>
2020-09-27 22:06:09 +0900

    first commit

---
+++ docker/src/main/java/com/jun/test/controller/DockerController.java
@@ -0,0 +1,13 @@
+package com.jun.test.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DockerController {
+
+	@GetMapping(value = "/docker")
+	public String docker() {
+		return "docker!";
+	}
+}
