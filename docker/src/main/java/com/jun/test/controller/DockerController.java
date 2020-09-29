b3717e5bc07c72b923d514d46046c37d951b072d
junsoo <wnstn212@gmail.com>
2020-09-30 02:54:16 +0900

    test

--- docker/src/main/java/com/jun/test/controller/DockerController.java
+++ docker/src/main/java/com/jun/test/controller/DockerController.java
@@ -10,10 +10,12 @@ public class DockerController {
 	public String docker() {
 		System.out.println("hohoho");
 		System.out.println("hohoho");
+		System.out.println("hohoho");
 		return "hohoho";
 	}
 	@GetMapping(value = "/hehe")
 	public String hehe() {
+		System.out.println("hohoho");
 		System.out.println("hohoho");
 		System.out.println("hohoho");
 		return "hehe";
